ccbad6cccf7b587c413f0343d8a4fce235592690
Jeremy Singer-Vine <jsvine@gmail.com>
2020-10-23 18:48:13 -0400

    Add plugin-installer to extending-visidata

--- terminal/scripts/extending.ts
+++ terminal/scripts/extending.ts
@@ -10,3 +10,7 @@ CAPTURE terminal/output/extending-01-longname.output
 ENTER
 AWAIT "4 rows  $" --start-line -1
 CAPTURE terminal/output/extending-02-deduped.output
+
+SEND " open-plugins" --enter
+AWAIT "\d+ plugins  $" --start-line -1
+CAPTURE terminal/output/extending-03-installer.output
